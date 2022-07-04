8108401db91b4697766bcb70e9a206a6526b0b0d
Kauan Curbani <Crbnii2020@gmail.com>
2022-07-04 09:35:39 -0300

    add home

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import User from "./pages/user/user";
 
 import React, { useState, useContext, createContext } from "react";
 import MyContext from "./Context/context";
+import Home from "./pages/homepage/home";
 
 function App() {
   const [user, setUser] = useState('')
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<LoginPage />} />
           <Route path="/user" element={<User />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </MyContext.Provider>
     </div>
--- src/pages/homepage/home.js
+++ src/pages/homepage/home.js
@@ -1,6 +1,12 @@
 
 function Home() {
 
+
+    return(
+        <div>
+            <h1>test</h1>
+        </div>
+    )
 }
 
 export default Home
